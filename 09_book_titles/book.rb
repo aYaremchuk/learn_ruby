74c90dc68b2d666a3cb112e4a2915d82ea9f3bc0
JAV <jaremchuk.andrij@gmail.com>
2015-03-23 22:09:17 +0200

    08 - task temperature_object done

---
+++ 09_book_titles/book.rb
@@ -0,0 +1,19 @@
+class Book
+  attr_accessor :title
+
+  def title=(title)
+    little_words = %w[the and in of a an]
+    @title = []
+    temp_result = title.split(/\W+/)
+    i = 0
+    temp_result.each do |x|
+      if !little_words.include?(x) || i == 0
+        @title << x.capitalize
+      else
+        @title << x
+      end
+      i += 1
+    end
+    @title = @title.join(" ")
+  end
+end
